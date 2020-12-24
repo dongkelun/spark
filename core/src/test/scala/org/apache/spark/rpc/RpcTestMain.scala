77a93cc76f62354e6ef36aac12fe1b0741beae5b
dongkelun <1412359494@qq.com>
2020-12-24 14:48:35 +0800

    Rpc测试demo

---
+++ core/src/test/scala/org/apache/spark/rpc/RpcTestMain.scala
@@ -0,0 +1,50 @@
+package org.apache.spark.rpc
+
+import org.apache.spark.{SecurityManager, SparkConf, SparkEnv}
+import org.apache.spark.rpc.netty.NettyRpcEnvFactory
+import org.scalatest.concurrent.Eventually.{eventually, interval, timeout}
+
+import scala.concurrent.duration._
+
+/**
+ * Created by dongkelun on 2020/12/24 11:20
+ */
+object RpcTestMain {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+    val env = createRpcEnv(conf, "local", 0)
+    @volatile var message: String = null
+    val rpcEndpointRef = env.setupEndpoint("send-locally", new RpcEndpoint {
+      override val rpcEnv = env
+
+      override def receive = {
+        case msg: String => {
+          println(msg)
+          message = msg
+        }
+      }
+    })
+    rpcEndpointRef.send("hello")
+
+    eventually(timeout(5.seconds), interval(10.milliseconds)) {
+      assert("hello" == message)
+    }
+
+    if (env != null) {
+      env.shutdown()
+    }
+    SparkEnv.set(null)
+
+  }
+
+  def createRpcEnv(
+                    conf: SparkConf,
+                    name: String,
+                    port: Int,
+                    clientMode: Boolean = false): RpcEnv = {
+    val config = RpcEnvConfig(conf, "test", "localhost", "localhost", port,
+      new SecurityManager(conf), 0, clientMode)
+    new NettyRpcEnvFactory().create(config)
+  }
+
+}
